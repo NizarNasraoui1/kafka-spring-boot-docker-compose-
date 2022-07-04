b50b5cfe295e237bb3ae4ef5853c1a13e90a474f
NizarNasraoui1 <nizar.al.nasraoui@gmail.com>
2022-07-04 15:08:06 +0200

    first

---
+++ src/main/java/com/example/demo/KafkaDemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class KafkaDemoApplication implements ApplicationRunner {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String topic,String msg) {
+        kafkaTemplate.send(topic, msg);
+    }
+
+    @KafkaListener(topics = "topic", groupId = "group-id")
+    public void listenToToto(String message) {
+        System.out.println("Received Messasge in group - group-id: " + message);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        sendMessage("topic","Hi Kafka");
+    }
+}
